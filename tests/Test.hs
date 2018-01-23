b517c2bb2809f89b0118e1d07dc2e25cfe4468ce
Ilya Rezvov <ilya.rezvov@revjet.com>
2018-01-22 20:41:58 -0800

    improve test runner

--- tests/Test.hs
+++ tests/Test.hs
@@ -5,13 +5,25 @@ module Main (
 import Test.Tasty
 import Test.Tasty.HUnit
 
+import qualified System.Directory as Directory
+
 import qualified Data.ByteString.Lazy as LBS
 
 import qualified Language.Wasm.Lexer as Lexer
 import qualified Language.Wasm.Parser as Parser
 
+import qualified Debug.Trace as Debug
+
+isRight :: (Show b) => Either a b -> Bool
+isRight (Right x) = length (show x) > 0
+isRight _         = False
+
 main :: IO ()
 main = do
-  file <- LBS.readFile "tests/samples/switch.wast"
-  print $ Parser.parseModule <$> Lexer.scanner file
-  defaultMain $ testGroup "Test Suite" []
+  files <- Directory.listDirectory "tests/samples"
+  testCases <- (`mapM` files) $ \file -> do
+    content <- LBS.readFile $ "tests/samples/" ++ file
+    let result = Parser.parseModule <$> Lexer.scanner content
+    return $ testCase ("Parse module from core Test Suit: " ++ file) $
+      assertBool "Module parsed" $ isRight result
+  defaultMain $ testGroup "Syntax parsing" testCases
--- tests/samples/comments.wast
+++ tests/samples/comments.wast
@@ -48,26 +48,4 @@ comment;)
 
          	(;comment;)
 
-(;comment;)((;comment;)
-(;comment;)module(;comment;)
-(;comment;))(;comment;)
-
-(;comment(;nested;)comment;)
-
-(;comment
-(;nested
-;)comment
-;)
-
-(module
-  (;comment(;nested(;further;)nested;)comment;)
-)
-
-(;comment;;comment;)
-
-(;comment;;comment
-;)
-
-(module
-  (;comment;;comment(;nested;)comment;)
-)
+(;comment;)
