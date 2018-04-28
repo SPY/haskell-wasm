845b57f428a2271da803dcefaff32a63e959195e
Ilya Rezvov <ilya.rezvov@revjet.com>
2018-04-28 14:24:26 -0700

    add config datatype

--- exec/Main.hs
+++ exec/Main.hs
@@ -1,5 +1,10 @@
 module Main where
 
+data WasmConfig = WasmConfig {
+    inputFile :: String,
+    outputFile :: String
+} deriving (Show, Eq)
+
 main :: IO ()
 main = do
     putStrLn "There is no WASM!"
