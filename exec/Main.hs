c5daf5b1875fc5f5c3ea4974c478a6e57a60c613
Ilya Rezvov <ilya.rezvov@revjet.com>
2018-04-28 14:31:40 -0700

    add complie function

--- exec/Main.hs
+++ exec/Main.hs
@@ -1,4 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
+
+import qualified Data.ByteString.Lazy as LBS
+
+import qualified Language.Wasm.Lexer as Lexer
+import qualified Language.Wasm.Parser as Parser
+import qualified Language.Wasm.Binary as Binary
 
 data OutputMode = WasmBinary | JSWrapper deriving (Show, Eq)
 
@@ -7,6 +14,15 @@ data WasmConfig = WasmConfig {
     outputFile :: String,
     outputMode :: OutputMode
 } deriving (Show, Eq)
+
+compile :: String -> String -> IO ()
+compile input output = do
+  content <- LBS.readFile input
+  case Lexer.scanner content >>= Parser.parseModule of
+    Right mod ->
+      LBS.writeFile output $ Binary.dumpModuleLazy mod
+    Left reason ->
+      putStrLn $ "Cannot complie module: " ++ reason
 
 main :: IO ()
 main = do
