1cf5b34d0739c31fb433336684f713c7b10c892f
Ilya Rezvov <ilya.rezvov@revjet.com>
2018-04-28 14:26:07 -0700

    add output mode

--- exec/Main.hs
+++ exec/Main.hs
@@ -1,8 +1,11 @@
 module Main where
 
+data OutputMode = WasmBinary | JSWrapper deriving (Show, Eq)
+
 data WasmConfig = WasmConfig {
     inputFile :: String,
-    outputFile :: String
+    outputFile :: String,
+    outputMode :: OutputMode
 } deriving (Show, Eq)
 
 main :: IO ()
