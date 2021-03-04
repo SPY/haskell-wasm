89df0e43d648968c166e6c085802473376ed5b42
Ilya Rezvov <ilya.rezvov@revjet.com>
2021-03-03 22:08:18 -0800

    add parsing of canonical nan, reinterpret instructions and check for mem idents uniqueness

--- tests/Test.hs
+++ tests/Test.hs
@@ -17,6 +17,7 @@ import qualified Data.List as List
 main :: IO ()
 main = do
   files <- filter (List.isSuffixOf ".wast") <$> Directory.listDirectory "tests/spec"
+  -- let files = ["call.wast"]
   scriptTestCases <- (`mapM` files) $ \file -> do
     test <- LBS.readFile ("tests/spec/" ++ file)
     return $ testCase file $ do
