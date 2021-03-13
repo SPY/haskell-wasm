f92754b5727d5a49c9eeaaa937bda9076316b2a2
Ilya Rezvov <ilya.rezvov@revjet.com>
2021-03-12 22:34:42 -0800

    change instansiating to be stateful

--- src/Language/Wasm/Script.hs
+++ src/Language/Wasm/Script.hs
@@ -219,7 +219,7 @@ runScript onAssertFail script = do
                     if failureString `elem` getFailureString reason
                     then return ()
                     else
-                        let msg = pos ++ ": Module is invalid for other reason. Expected "
+                        let msg = "Module is invalid for other reason. Expected "
                                 ++ show failureString
                                 ++ ", but actual is "
                                 ++ show (getFailureString reason)
