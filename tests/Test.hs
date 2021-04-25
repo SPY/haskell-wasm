825a24d59cfa84b9b08afcda420d361f71e617f3
Ilya Rezvov <ilya.rezvov@revjet.com>
2021-04-25 12:24:47 -0700

    add reference types test to test suite

--- tests/Test.hs
+++ tests/Test.hs
@@ -16,14 +16,20 @@ import qualified Data.List as List
 
 main :: IO ()
 main = do
-  files <- filter (List.isSuffixOf ".wast") <$> Directory.listDirectory "tests/spec"
-  -- let files = ["const.wast"]
-  scriptTestCases <- (`mapM` files) $ \file -> do
-    test <- LBS.readFile ("tests/spec/" ++ file)
-    return $ testCase file $ do
-      case Wasm.parseScript test of
-        Right script -> 
-          Script.runScript (\msg assert -> assertFailure ("Failed assert: " ++ msg ++ ". Assert " ++ show assert)) script
-        Left error ->
-          assertFailure $ "Failed to parse with error: " ++ show error
-  defaultMain $ testGroup "Wasm Core Test Suit" scriptTestCases
+  let groups = [
+          ("Core Tests", "tests/spec"),
+          ("Reference Types Proposal", "tests/spec/proposals/reference-types")
+        ]
+  testGroups <- (`mapM` groups) $ \(groupName, dir) -> do
+    files <- filter (List.isSuffixOf ".wast") <$> Directory.listDirectory dir
+    -- let files = ["const.wast"]
+    scriptTestCases <- (`mapM` files) $ \file -> do
+      test <- LBS.readFile (dir ++ "/" ++ file)
+      return $ testCase file $ do
+        case Wasm.parseScript test of
+          Right script -> 
+            Script.runScript (\msg assert -> assertFailure ("Failed assert: " ++ msg ++ ". Assert " ++ show assert)) script
+          Left error ->
+            assertFailure $ "Failed to parse with error: " ++ show error
+    return $ testGroup groupName scriptTestCases
+  defaultMain $ testGroup "Wasm Test Suit" testGroups
